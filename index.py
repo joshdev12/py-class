8b96075ead54d93fac968757cf22da3888e4a7d8
joshdev <joshuaajayi012@gmail.com>
2022-09-20 12:33:24 +0100

    py

---
+++ index.py
@@ -0,0 +1,94 @@
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+
+# print(f"Hello, {name}... You're {age} years old.")
+"""
+int() -> integer
+str() -> string
+bool() -> boolean
+float() -> floating numbers
+"""
+
+
+# name = ""
+
+# convt = bool(name)
+
+
+# print("Converted: ", convt)
+# print("Type of convt: ", type(convt))
+
+
+
+
+
+
+# names = (1)
+# # names[0] = "AY"
+
+# print(names)
+# print(type(names))
+
+
+# name = input("Enter yor name: ").title()
+
+# if name == "Ade":
+#     print(f"Name is {name}")
+# elif name == "Sola":
+#     print(f"Name is {name}")
+# else:
+#     print("No name")
+
+# another = False
+# name = "Ade" if another else "Sola"
+
+# print(name)
+
+# FOR LOOP
+# name = "PraiseGod"
+# for i in name:
+#     print(i)
+
+
+# import turtle
+
+# t = turtle.Pen()
+# t.shape("turtle")
+
+# for i in range(2000):
+#     t.forward(i)
+#     t.left(90)
+
+# for i in range(1, 13):
+#     print(f"6 x {i} = {6 * i}")
+
+# count = 1
+# while True:
+#     count += 1
+
+#     if count == 20:
+#         break
+
+#     print(count)
+
+
+import os
+
+for i in range(15):
+    os.system("start")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,33 @@
+# # name = "Vic"
+
+# # print(name)
+
+# # print(dir(name))
+# # print()
+# # print(type(name))
+# # print()
+# # print(name.upper())
+# # print(len(name))
+
+
+# # print("my name is {1} {0}".format("Adesanmi", "PG"))
+
+# name = "PraiseGod"
+
+# print(name[6:9])
+# a
+
+
+"""
+ARITHMETIC OP -> //
+COMPARISON OP
+LOGICAL
+MEMBERSHIP
+
+
+
+"""
+
+
+print(10 % 3)
+print(10 // 3)
